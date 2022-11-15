140ea3e7f022cd38ff26dce6bba017d0ca574aa0
I551967 <andreea.bolea@sap.com>
2022-11-15 15:38:14 +0200

    init commit

---
+++ utils/file_utils.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import re
+from glob import glob
+from shutil import copyfile
+from typing import List, Tuple
+
+FILENAME_REGEX = r"(\d+)_(\d+)\.jpg"
+# FILENAME_REGEX = r"Img_(\d+)_(\d+)_(\d+)\.jpg"
+
+def get_file_name(file_path: str) -> str:
+    return file_path.split("/")[-1]
+
+
+def extract_user_sample_ids(image_path: str) -> Tuple[str, str]:
+    """
+    Extract user ID and sample number from filename
+    """
+    image_filename = get_file_name(image_path)
+    user_id, sample_id = re.match(FILENAME_REGEX, image_filename).groups()
+    return user_id, sample_id
+
+
+def create_empty_dir(target_dir: str):
+    """
+    Create an empty directory, removing all files first if the target directory
+    already exists.
+    """
+    if os.path.exists(target_dir):
+        print('here2')
+        files = glob(target_dir + "/*")
+        for file in files:
+            os.remove(file)
+    else:
+        print('here3')
+        os.makedirs(target_dir, 0o666)
+
+
+def copy_dataset(file_paths: List[str], target_dir: str):
+    for file_path in file_paths:
+        file_name = get_file_name(file_path)
+        copyfile(file_path, f"{target_dir}/{file_name}")
